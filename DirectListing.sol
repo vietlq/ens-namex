03779282fba431650c00fe6a91a02b33719783a6
Le Quoc Viet <vietlq85@gmail.com>
2018-08-11 13:08:32 +0100

    Create DirectListing.sol

---
+++ DirectListing.sol
@@ -0,0 +1,30 @@
+pragma solidity 0.4.24;
+
+contract DirectListing {
+    event Offered(bytes32 indexed node, address indexed owner, uint256 price, uint256 ttl);
+    event Bought(bytes32 indexed node, address indexed newOwner, uint256 price);
+    event Canceled(bytes32 indexed node);
+    
+    ENS ens;
+    Registrar registrar;
+    
+    constructor() {
+        
+    }
+    
+    function isOffered(bytes32 _node) external view returns (bool) {
+        
+    }
+    
+    function offer(bytes32 _node, uint256 _price, uint256 _ttl) external {
+        
+    }
+    
+    function cancel(bytes32 _node) external {
+        
+    }
+    
+    function buy(bytes32 _node) external payable {
+        
+    }
+}
