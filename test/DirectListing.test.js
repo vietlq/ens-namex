25655b97443ea1cff5e3cad81b9d7ca5d4289902
Evangelos Pappas <epappas@evalonlabs.com>
2018-08-11 18:13:59 +0100

    ENS TLD - progress

--- test/DirectListing.test.js
+++ test/DirectListing.test.js
@@ -23,42 +23,61 @@ contract('DirectListing', function (accounts) {
 
     it('Offer', async () => {
         const ens = await ENS.new();
-        const registrar = await Registrar.new(ens.address, 0, 0);
+
+
+        const registrar = await Registrar.new(ens.address, web3.sha3('eth'), 0);
+
+
+        await ens.setSubnodeOwner(0, web3.sha3('eth'), registrar.address);
         const contract = await DirectListing.new(registrar.address);
 
-        //console.log('web3 => ', web3);
-        //console.log(web3);
-        const TIME_80_WEEKS = 80*7*24*60*60;
-        const advance80weeksResult = await sendRpc('evm_increaseTime', [TIME_80_WEEKS]);
-        console.log("advance80weeksResult => ", advance80weeksResult);
-        //assert(parseInt(advance80weeksResult.result) == TIME_80_WEEKS);
+        const TIME_80_WEEKS = 80 * 7 * 24 * 60 * 60;
+        const TIME_5_DAYS = 5 * 24 * 60 * 60 + 1;
+        await sendRpc('evm_increaseTime', [TIME_80_WEEKS]);
 
-        const event = contract.Offered({
-            _from: accounts[0]
-        }, {
-            fromBlock: 0,
-            toBlock: 'latest'
-        });
-
+        const node = web3.sha3('testingname');
+        const nodeEntryBeforeAuction = (await registrar.entries(node))[0];
         const price = 10;
-        const node = '0x0123000000000000000000000000000000000000000000000000000000000000';
 
-        const p1 = new Promise((resolve, reject) => {
-            event.watch((error, result) => {
-                if (error) return reject(error);
+        assert(nodeEntryBeforeAuction.toString() === '0');
 
-                resolve(result);
-            });
-        }).then((result) => {
-            assert.equal(result.args.owner, accounts[0], 'Should have matched the creator');
-            assert.equal(result.args.price, price, 'Should have matched the offered price');
-            assert.equal(result.args.node, node, 'Should have matched the matched label node');
+        await registrar.startAuction(node, {
+            from: accounts[0],
+            gas: 100000
         });
 
-        const p2 = contract.offer(node, price, 10, {
-            from: accounts[0]
-        });
+        const nodeEntryAfterAuction = (await registrar.entries(node))[0];
 
-        return Promise.all([p1, p2]);
+        assert(nodeEntryBeforeAuction.toString() === '1');
+
+        await sendRpc('evm_increaseTime', [TIME_5_DAYS]);
+
+        assert(nodeEntryAfterAuction.toString() === '4');
+
+
+        // const event = contract.Offered({
+        //     _from: accounts[0]
+        // }, {
+        //     fromBlock: 0,
+        //     toBlock: 'latest'
+        // });
+
+        // const p1 = new Promise((resolve, reject) => {
+        //     event.watch((error, result) => {
+        //         if (error) return reject(error);
+
+        //         resolve(result);
+        //     });
+        // }).then((result) => {
+        //     assert.equal(result.args.owner, accounts[0], 'Should have matched the creator');
+        //     assert.equal(result.args.price, price, 'Should have matched the offered price');
+        //     assert.equal(result.args.node, node, 'Should have matched the matched label node');
+        // });
+
+        // const p2 = contract.offer(node, price, 10, {
+        //     from: accounts[0]
+        // });
+
+        // return Promise.all([p1, p2]);
     });
 });
