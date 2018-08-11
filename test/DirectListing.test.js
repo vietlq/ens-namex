5915d55beef91056575ebe3ed198eafb9cbec703
Evangelos Pappas <epappas@evalonlabs.com>
2018-08-11 16:27:43 +0100

    async/await tests

--- test/DirectListing.test.js
+++ test/DirectListing.test.js
@@ -4,40 +4,37 @@ var ENS = artifacts.require('./ENSRegistry');
 
 contract('DirectListing', function (accounts) {
 
-    it('Offer', () => {
-        return ENS.new().then(ens => {
-            return Registrar.new(ens.address, 0, 0);
-        }).then(registrar => {
+    it('Offer', async () => {
+        const ens = await ENS.new();
+        const registrar = await Registrar.new(ens.address, 0, 0);
+        const contract = await DirectListing.new(registrar.address);
 
-            return DirectListing.new(registrar.address).then((contract) => {
-                const event = contract.Offered({
-                    _from: accounts[0]
-                }, {
-                    fromBlock: 0,
-                    toBlock: 'latest'
-                });
-
-                const price = 10;
-                const node = '0x0123';
-
-                const p1 = new Promise((resolve, reject) => {
-                    event.watch((error, result) => {
-                        if (error) return reject(error);
-
-                        resolve(result);
-                    });
-                }).then((result) => {
-                    assert.equal(result.args.owner, accounts[0], 'Should have matched the creator');
-                    assert.equal(result.args.price, price, 'Should have matched the offered price');
-                    assert.equal(result.args.node, node, 'Should have matched the matched label node');
-                });
-
-                const p2 = contract.offer(node, price, 10, {
-                    from: accounts[0]
-                });
-
-                return Promise.all([p1, p2]);
-            });
+        const event = contract.Offered({
+            _from: accounts[0]
+        }, {
+            fromBlock: 0,
+            toBlock: 'latest'
         });
+
+        const price = 10;
+        const node = '0x0123';
+
+        const p1 = new Promise((resolve, reject) => {
+            event.watch((error, result) => {
+                if (error) return reject(error);
+
+                resolve(result);
+            });
+        }).then((result) => {
+            assert.equal(result.args.owner, accounts[0], 'Should have matched the creator');
+            assert.equal(result.args.price, price, 'Should have matched the offered price');
+            assert.equal(result.args.node, node, 'Should have matched the matched label node');
+        });
+
+        const p2 = contract.offer(node, price, 10, {
+            from: accounts[0]
+        });
+
+        return Promise.all([p1, p2]);
     });
 });
