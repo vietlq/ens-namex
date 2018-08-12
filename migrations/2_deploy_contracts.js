2f967cfa28f38feb6c2cabbfd84a4b648b44c499
Evangelos Pappas <epappas@evalonlabs.com>
2018-08-12 14:57:17 +0100

    Simplifying the truffle deploy part

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,8 @@
 // https://github.com/poanetwork/solidity-flattener
-
 const ENS = artifacts.require("@ensdomains/ens/contracts/ENSRegistry");
 const FIFSRegistrar = artifacts.require('./ens/Registrar');
 const DirectListing = artifacts.require('./DirectListing');
 
-// Currently the parameter('./ContractName') is only used to imply
-// the compiled contract JSON file name. So even though `Registrar.sol` is
-// not existed, it's valid to put it here.
-// TODO: align the contract name with the source code file name.
 const Registrar = artifacts.require('@ensdomains/ens/contracts/Registrar');
 const web3 = new(require('web3'))();
 const namehash = require('eth-ens-namehash').hash;
@@ -24,89 +19,20 @@ function getRootNodeFromTLD(tld) {
     };
 }
 
-/**
- * Deploy the ENS and FIFSRegistrar
- *
- * @param {Object} deployer truffle deployer helper
- * @param {string} tld tld which the FIFS registrar takes charge of
- */
-function deployFIFSRegistrar(deployer, tld) {
-    var rootNode = getRootNodeFromTLD(tld);
-
-    // Deploy the ENS first
-    deployer.deploy(ENS)
-        .then(() => {
-            // Deploy the FIFSRegistrar and bind it with ENS
-            return deployer.deploy(FIFSRegistrar, ENS.address, rootNode.namehash);
-        })
-        .then(function () {
-            // Transfer the owner of the `rootNode` to the FIFSRegistrar
-            ENS.at(ENS.address).setSubnodeOwner('0x0', rootNode.sha3, FIFSRegistrar.address);
-        });
-}
-
-/**
- * Deploy the ENS and HashRegistrar(Simplified)
- *
- * @param {Object} deployer truffle deployer helper
- * @param {string} tld tld which the Hash registrar takes charge of
- */
-function deployAuctionRegistrar(deployer, tld) {
-    var rootNode = getRootNodeFromTLD(tld);
-
-    // Deploy the ENS first
-    deployer.deploy(ENS)
-        .then(() => {
-            // Deploy the HashRegistrar and bind it with ENS
-            // The last argument `0` specifies the auction start date to `now`
-            return deployer.deploy(Registrar, ENS.address, rootNode.namehash, 0);
-        })
-        .then(function () {
-            // Transfer the owner of the `rootNode` to the HashRegistrar
-            ENS.at(ENS.address).setSubnodeOwner('0x0', rootNode.sha3, Registrar.address);
-        });
-}
-
-/**
- * Deploy the ENS and HashRegistrar(Simplified)
- *
- * @param {Object} deployer truffle deployer helper
- * @param {string} tld tld which the Hash registrar takes charge of
- */
-function deployAuctionRegistrarAndDirectListing(deployer, tld) {
-    var rootNode = getRootNodeFromTLD(tld);
-
-    // Deploy the ENS first
-    deployer.deploy(ENS)
-        .then(() => {
-            // Deploy the HashRegistrar and bind it with ENS
-            // The last argument `0` specifies the auction start date to `now`
-            return deployer.deploy(Registrar, ENS.address, rootNode.namehash, 0);
-        })
-        .then(function () {
-            // Transfer the owner of the `rootNode` to the HashRegistrar
-            return ENS.at(ENS.address).setSubnodeOwner('0x0', rootNode.sha3, Registrar.address);
-        })
-        .then(function () {
-            // Deploy the DirectListing contract
-            console.log('ENS.address => ', ENS.address);
-            console.log('Registrar.address => ', ENS.address);
-            return deployer.deploy(DirectListing, Registrar.address);
-        });
-}
-
-module.exports = function (deployer, network) {
+module.exports = async function (deployer, network) {
     // Use TLD .namex because .eth is already taken
     var tld = 'namex';
     console.log('network => ', network);
 
-    if (network === 'dev.fifs') {
-        deployFIFSRegistrar(deployer, tld);
-    } else if (network === 'dev.auction') {
-        deployAuctionRegistrar(deployer, tld);
-    } else if (network === 'development') {
-        deployAuctionRegistrarAndDirectListing(deployer, tld);
-        console.log('DEPLOYED!');
-    }
+    var rootNode = getRootNodeFromTLD(tld);
 
+    await deployer.deploy(ENS);
+    await deployer.deploy(Registrar, ENS.address, rootNode.namehash, 0);
+    await ENS.at(ENS.address).setSubnodeOwner('0x0', rootNode.sha3, Registrar.address);
+
+    // Deploy the DirectListing contract
+    console.log('ENS.address => ', ENS.address);
+    console.log('Registrar.address => ', ENS.address);
+
+    await deployer.deploy(DirectListing, Registrar.address);
 };
