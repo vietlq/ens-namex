77c8e455dd42d0efa62bc85c264db4c4e02a01d3
sprusr <scott@spru.sr>
2018-08-12 12:22:40 +0100

    Implement more UI

--- src/Name.js
+++ src/Name.js
@@ -1,59 +1,109 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import { Alert, Table } from 'react-bootstrap'
-// import { drizzleConnect } from 'drizzle-react'
+import { Alert, Table, Button } from 'react-bootstrap'
+import { drizzleConnect } from 'drizzle-react'
 import CustomContractData from './CustomContractData'
-import { utf8ToHex } from 'web3-utils'
+import { sha3 } from 'web3-utils'
+import PropTypes from 'prop-types'
 
 const transformStatus = value => {
   return !!value.price ? <span>For sale</span> : <span>Unknown</span>
 }
 
-const Name = ({ match }) => (
-  <div>
-    <h2>{match.params.name}</h2>
-    <Alert bsStyle="info">
-      You own this domain. Want to <Link to={`/name/${match.params.name}/sell`}>sell it</Link>?
-    </Alert>
-    <Alert bsStyle="info">
-      You own this domain, and have listed it for sale. Want to <Link to={`/name/${match.params.name}/sell`}>cancel the sale</Link>?
-    </Alert>
-    <Alert bsStyle="warning">
-      This domain is not for sale.
-    </Alert>
-    <Alert bsStyle="success">
-      This domain is for sale! Put in an offer.
-    </Alert>
-    <Table bordered condensed>
-      <tbody>
-        <tr>
-          <th>Status</th>
-          <td><CustomContractData contract="DirectListing" method="offerings" methodArgs={[utf8ToHex(match.params.name)]} transform={transformStatus} /></td>
-        </tr>
-        <tr>
-          <th>Owner</th>
-          <td><CustomContractData contract="DirectListing" method="offerings" methodArgs={[utf8ToHex(match.params.name)]} returnKey="nodeOwner" /></td>
-        </tr>
-        <tr>
-          <th>Price</th>
-          <td><CustomContractData contract="DirectListing" method="offerings" methodArgs={[utf8ToHex(match.params.name)]} returnKey="price" /></td>
-        </tr>
-        <tr>
-          <th>Expires at</th>
-          <td><CustomContractData contract="DirectListing" method="offerings" methodArgs={[utf8ToHex(match.params.name)]} returnKey="expireAt" /></td>
-        </tr>
-      </tbody>
-    </Table>
-  </div>
-)
+function namehash(name) {
+  var node = '0x0000000000000000000000000000000000000000000000000000000000000000';
+  if (name != '') {
+      var labels = name.split(".");
+      for(var i = labels.length - 1; i >= 0; i--) {
+          node = sha3(node + sha3(labels[i]).slice(2), {encoding: 'hex'});
+      }
+  }
+  return node.toString();
+}
 
-// const mapStateToProps = state => {
-//   return {
-//     drizzleStatus: state.drizzleStatus,
-//     SimpleStorage: state.contracts.DirectListing
-//   }
-// }
+function labelhash(label) {
+  return sha3(label.slice(0, -4))
+}
 
-// export default drizzleConnect(Name, mapStateToProps)
+class Name extends Component {
+  render() {
+    return (
+      <div>
+        <h2>{this.props.match.params.name}</h2>
+        {<CustomContractData contract="DirectListing" method="offerings" methodArgs={[labelhash(this.props.match.params.name)]} render={
+          data => data.nodeOwner === this.props.accounts[0] && !Number(data.price) (
+            <Alert bsStyle="info">
+              You own this domain. Want to <Link to={`/name/${this.props.match.params.name}/sell`}>sell it</Link>?
+            </Alert>
+          )} />
+        || <CustomContractData contract="ENSRegistry" method="owner" methodArgs={[namehash(this.props.match.params.name)]} render={
+        data => data.owner === this.props.accounts[0] && !Number(data.price) (
+          <Alert bsStyle="info">
+            You own this domain. Want to <Link to={`/name/${this.props.match.params.name}/sell`}>sell it</Link>?
+          </Alert>
+        )} />}
+        <CustomContractData contract="DirectListing" method="offerings" methodArgs={[labelhash(this.props.match.params.name)]} render={
+          data => data.nodeOwner === this.props.accounts[0] && !!Number(data.price) && (
+            <Alert bsStyle="info">
+              You own this domain, and have listed it for sale. Want to <Link to={`/name/${this.props.match.params.name}/sell`}>cancel the sale</Link>?
+            </Alert>
+          )} />
+        <CustomContractData contract="DirectListing" method="offerings" methodArgs={[labelhash(this.props.match.params.name)]} render={
+          data => data.nodeOwner !== this.props.accounts[0] && !Number(data.price) && (
+            <Alert bsStyle="warning">
+              This domain is not for sale.
+            </Alert>
+          )} />
+        <CustomContractData contract="DirectListing" method="offerings" methodArgs={[labelhash(this.props.match.params.name)]} render={
+          data => data.nodeOwner !== this.props.accounts[0] && !!Number(data.price) && (
+            <Alert bsStyle="success">
+              This domain is for sale!
+            </Alert>
+          )} />
+        <Table bordered condensed>
+          <tbody>
+            <tr>
+              <th>Status</th>
+              <td><CustomContractData contract="DirectListing" method="isOffered" methodArgs={[labelhash(this.props.match.params.name)]} render={transformStatus} /></td>
+            </tr>
+            <tr>
+              <th>Owner {this.props.accounts[0]}</th>
+              <td><CustomContractData contract="DirectListing" method="offerings" methodArgs={[labelhash(this.props.match.params.name)]} returnKey="nodeOwner" /></td>
+            </tr>
+            <tr>
+              <th>Price</th>
+              <td><CustomContractData contract="DirectListing" method="offerings" methodArgs={[labelhash(this.props.match.params.name)]} returnKey="price" /></td>
+            </tr>
+            <tr>
+              <th>Expires at</th>
+              <td><CustomContractData contract="DirectListing" method="offerings" methodArgs={[labelhash(this.props.match.params.name)]} returnKey="expireAt" /></td>
+            </tr>
+          </tbody>
+        </Table>
+        {console.log(this.context)}
+        <CustomContractData contract="DirectListing" method="offerings" methodArgs={[labelhash(this.props.match.params.name)]} render={
+          data => data.nodeOwner !== this.props.accounts[0] && !!Number(data.price) && (
+            <Button bsStyle="primary" bsSize="large" onClick={() => this.context.drizzle.contracts.DirectListing.methods.buy(labelhash(this.props.match.params.name)).send({ value: data.price })}>
+              Buy
+            </Button>
+          )} />
+      </div>
+    )
+  }
+}
 
-export default Name
+Name.contextTypes = {
+  drizzle: PropTypes.object
+}
+
+const mapStateToProps = state => {
+  return {
+    drizzleStatus: state.drizzleStatus,
+    DirectListing: state.contracts.DirectListing,
+    accounts: state.accounts
+  }
+}
+
+export default drizzleConnect(Name, mapStateToProps)
+
+// export default Name
--- src/Sell.js
+++ src/Sell.js
@@ -1,40 +1,61 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Alert, Table, FormGroup, FormControl, InputGroup, Button, Form } from 'react-bootstrap'
+import { sha3 } from 'web3-utils'
+import PropTypes from 'prop-types'
 
-const Sell = ({ match }) => (
-  <div>
-    <h2>Sell {match.params.name}</h2>
-    <Alert bsStyle="danger">
-      You do not own this domain.
-    </Alert>
-    <Table bordered condensed>
-      <tbody>
-        <tr>
-          <th>Status</th>
-          <td>For sale</td>
-        </tr>
-        <tr>
-          <th>Other info</th>
-          <td>Yes</td>
-        </tr>
-      </tbody>
-    </Table>
-    <Form inline>
-      <FormGroup bsSize="large">
-        <InputGroup>
-          <FormControl type="number" placeholder="Offer amount" />
-          <InputGroup.Addon>ETH</InputGroup.Addon>
-        </InputGroup>{ ' ' }
-        <InputGroup>
-          <FormControl type="number" placeholder="TTL" />
-          <InputGroup.Addon>seconds</InputGroup.Addon>
-        </InputGroup>{ ' ' }
-        <Button bsStyle="primary" bsSize="large">
-          Submit
-        </Button>
-      </FormGroup>
-    </Form>
-  </div>
-)
+function labelhash(label) {
+  return sha3(label.slice(0, -4))
+}
+
+class Sell extends Component {
+  state = {}
+  handleAmountChange = event => {
+    this.setState({amount: event.target.value})
+  }
+  handleTimeChange = event => {
+    this.setState({time: event.target.value})
+  }
+  render() {
+    return (
+      <div>
+        <h2>Sell {this.props.match.params.name}</h2>
+        <Alert bsStyle="danger">
+          You do not own this domain.
+        </Alert>
+        <Table bordered condensed>
+          <tbody>
+            <tr>
+              <th>Status</th>
+              <td>For sale</td>
+            </tr>
+            <tr>
+              <th>Other info</th>
+              <td>Yes</td>
+            </tr>
+          </tbody>
+        </Table>
+        <Form inline>
+          <FormGroup bsSize="large">
+            <InputGroup>
+              <FormControl type="number" placeholder="Offer amount" onChange={this.handleAmountChange} />
+              <InputGroup.Addon>ETH</InputGroup.Addon>
+            </InputGroup>{ ' ' }
+            <InputGroup>
+              <FormControl type="number" placeholder="TTL" onChange={this.handleTimeChange} />
+              <InputGroup.Addon>seconds</InputGroup.Addon>
+            </InputGroup>{ ' ' }
+            <Button bsStyle="primary" bsSize="large" onClick={() => this.context.drizzle.contracts.DirectListing.methods.offer(labelhash(this.props.match.params.name), Number(this.state.amount), Number(this.state.time)).send()}>
+              Submit
+            </Button>
+          </FormGroup>
+        </Form>
+      </div>
+    )
+  }
+}
+
+Sell.contextTypes = {
+  drizzle: PropTypes.object
+}
 
 export default Sell
