fe881e427ca9c159f5b819d129d928a8c01afa47
Viet Le <vietlq85@gmail.com>
2018-08-19 19:11:54 +0100

    Moved Buy/Sell ops to Name

--- src/Name.js
+++ src/Name.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import { Alert, Table, Button } from 'react-bootstrap'
 import { drizzleConnect } from 'drizzle-react'
 import CustomContractData from './CustomContractData'
-import { sha3, fromWei } from 'web3-utils'
+import { sha3, fromWei, toWei } from 'web3-utils'
 import PropTypes from 'prop-types'
+import { Alert, Table, FormGroup, FormControl, InputGroup, Button, Form } from 'react-bootstrap'
 
 const ZERO_ADDR = '0x0000000000000000000000000000000000000000';
 const ZERO_UINT256 = '0x0000000000000000000000000000000000000000000000000000000000000000';
-
-const transformStatus = value => {
-  return !!value.price ? <span>For sale</span> : <span>Unknown</span>
-}
 
 export function namehash(name) {
   var node = ZERO_UINT256;
@@ -29,6 +25,43 @@ function labelhash(label) {
 }
 
 class Name extends Component {
+  handleAmountChange = event => this.setState({amount: event.target.value})
+  // TTL
+  handleTimeChange = event => this.setState({time: event.target.value})
+  transfer = async () => {
+    let Registrar = this.context.drizzle.contracts.Registrar;
+    let transferMethod = Registrar.methods.transfer;
+    let DirectListing = this.context.drizzle.contracts.DirectListing;
+    console.log('transferMethod => ');
+    console.log(transferMethod);
+
+    let transferMethodInstance = transferMethod(labelhash(this.props.match.params.name), DirectListing.address);
+    console.log('transferMethodInstance => ', transferMethodInstance);
+
+    transferMethodInstance.send().then((result) => {
+      console.log('result => ', result);
+    }).catch((error) => {
+      console.log('error => ', error);
+    });
+  }
+  offer = async () => {
+    let DirectListing = this.context.drizzle.contracts.DirectListing;
+    let offerMethod = DirectListing.methods.offer;
+    // TTL + current epoch (everything in seconds)
+    const expireAtEpoch = parseInt(this.state.time, 10) + Math.round((new Date()).getTime() / 1000);
+    const offerAmount = toWei(this.state.amount, "ether");
+    let offerMethodInstance = offerMethod(labelhash(this.props.match.params.name), offerAmount, Number(expireAtEpoch));
+
+    console.log('offerAmount => ', offerAmount);
+    console.log('About to offer the amount: ', this.state.amount);
+    console.log('About to offer the amount in Number format: ', Number(this.state.amount));
+
+    offerMethodInstance.send().then((result) => {
+      console.log('result => ', result);
+    }).catch((error) => {
+      console.log('error => ', error);
+    });
+  }
   render() {
     if (!this.props.match.params.name) return <p>Loading...</p>
     return (
@@ -51,6 +84,7 @@ class Name extends Component {
           }
         />
 
+        <h4>The DirectListing address: {this.context.drizzle.contracts.DirectListing.address}</h4>
         <h4>Your address: {this.props.accounts[0]}</h4>
 
         {
@@ -249,32 +283,107 @@ class Name extends Component {
 
         <CustomContractData
           contract="DirectListing"
-          method="offerings"
+          method="deedOwner"
           methodArgs={[labelhash(this.props.match.params.name)]}
+          accounts={this.props.accounts}
           render={
-            // TODO: Check against the deedOwner and deedPreviousOwner
-            data => (data.nodeOwner !== this.props.accounts[0]) && !!Number(data.price) && (
-              <Button bsStyle="primary" bsSize="large"
-                onClick={
-                  () => {
-                    let DirectListing = this.context.drizzle.contracts.DirectListing;
-                    let buyMethod = DirectListing.methods.buy;
-                    let buyMethodInstance = buyMethod(labelhash(this.props.match.params.name));
+            owner => {
+              // The account owns the domain, but hasn't transferred to the DirectListing contract
+              if (owner === this.props.accounts[0])
+              {
+                return (
+                  <Button bsStyle="primary" bsSize="large" onClick={this.transfer}>
+                    Transfer to Exchange
+                  </Button>
+                )
+              } else {
+                if (owner === this.context.drizzle.contracts.DirectListing.address) {
+                  return (
+                    <CustomContractData
+                      contract="DirectListing"
+                      method="deedPreviousOwner"
+                      methodArgs={[labelhash(this.props.match.params.name)]}
+                      accounts={this.props.accounts}
+                      render={
+                        previousOwner => {
+                          console.log('this.props.deedOwner => ', this.props.deedOwner);
+                          console.log('previousOwner => ', previousOwner);
+                          if (previousOwner === this.props.accounts[0]) {
+                            return (
+                              <Form inline>
+                                <FormGroup bsSize="large">
+                                  <InputGroup>
+                                    <FormControl
+                                      type="number"
+                                      placeholder="Offer amount"
+                                      onChange={this.handleAmountChange} />
+                                    <InputGroup.Addon>ETH</InputGroup.Addon>
+                                  </InputGroup>{ ' ' }
+                                  <InputGroup>
+                                    <FormControl type="number" placeholder="TTL" onChange={this.handleTimeChange} />
+                                    <InputGroup.Addon>seconds</InputGroup.Addon>
+                                  </InputGroup>{ ' ' }
+                                  <Button bsStyle="primary" bsSize="large" onClick={this.offer}>
+                                    Submit
+                                  </Button>
+                                </FormGroup>
+                              </Form>
+                            )
+                          } else {
+                            console.log("accounts => ", this.props.accounts);
+                            return (
+                              <div>
+                                <Alert bsStyle="danger">
+                                  You do not own this domain.
+                                </Alert>
 
-                    console.log('data.price => ', data.price);
-                    console.log('Number(data.price) => ', Number(data.price));
+                                <CustomContractData
+                                  contract="DirectListing"
+                                  method="offerings"
+                                  methodArgs={[labelhash(this.props.match.params.name)]}
+                                  render={
+                                    // TODO: Check against the deedOwner and deedPreviousOwner
+                                    data => (data.nodeOwner !== this.props.accounts[0]) && !!Number(data.price) && (
+                                      <Button bsStyle="primary" bsSize="large"
+                                        onClick={
+                                          () => {
+                                            let DirectListing = this.context.drizzle.contracts.DirectListing;
+                                            let buyMethod = DirectListing.methods.buy;
+                                            let buyMethodInstance = buyMethod(labelhash(this.props.match.params.name));
 
-                    buyMethodInstance.send({ value: data.price }).then((result) => {
-                      console.log('Successfully Bought the domain. Result: ', result);
-                    }).catch((error) => {
-                      console.log('Could not Buy. Error: ', error);
-                    });
-                  }
+                                            console.log('data.price => ', data.price);
+                                            console.log('Number(data.price) => ', Number(data.price));
+
+                                            buyMethodInstance.send({ value: data.price }).then((result) => {
+                                              console.log('Successfully Bought the domain. Result: ', result);
+                                            }).catch((error) => {
+                                              console.log('Could not Buy. Error: ', error);
+                                            });
+                                          }
+                                        }
+                                      >
+                                        Buy
+                                      </Button>
+                                    )
+                                  }
+                                />
+                              </div>
+                            )
+                          }
+                        }
+                      }
+                    />
+                  )
+                } else {
+                  return (
+                    <Alert bsStyle="danger">
+                      Both you and the DirectListing contract do not own the domain
+                        {this.props.match.params.name}.
+                    </Alert>
+                  )
                 }
-              >
-                Buy
-              </Button>
-            )
+              }
+            }
           }
         />
       </div>
